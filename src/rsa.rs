db84825e830a711f610f9fdff1703f93a98d0725
h3nnn4n <uber.renan@gmail.com>
2018-05-13 16:42:17 -0300

    mod_inv now supports bigint

--- src/rsa.rs
+++ src/rsa.rs
@@ -5,14 +5,23 @@ use self::rug::Integer;
 
 use big_primes;
 
-fn egcd(a: i64, b: i64) -> (i64, i64, i64) {
-    if a == 0 {
-        return (b, 0, 1);
-    } else {
-        let (g, x, y) = egcd(b % a, a);
+fn mod_inv(a: Integer, module: Integer) -> Integer {
+    let mut mn = (module.clone(), a.clone());
+    let mut xy = (Integer::from(0), Integer::from(1));
 
-        return (g, y - (b / a) * x, x);
+    while mn.1 != 0 {
+        xy = (
+            xy.1.clone(),
+            xy.0.clone() - Integer::from(&mn.0 / &mn.1) * xy.1.clone(),
+        );
+        mn = (mn.1.clone(), Integer::from(&mn.0 % &mn.1));
     }
+
+    while xy.0.clone() < 0 {
+        xy.0 = Integer::from(&xy.0 + &module);
+    }
+
+    xy.0
 }
 
 pub fn get_pq(n_bits: i64) -> (i64, i64) {
@@ -21,6 +30,7 @@ pub fn get_pq(n_bits: i64) -> (i64, i64) {
     let n = Integer::from(&p * &q);
     let tot = Integer::from(Integer::from(&p - 1) * Integer::from(&q - 1));
     let e = big_primes::get_prime_with_n_bits(8);
+    let d = mod_inv(e.clone(), tot.clone());
     (10, 10)
 }
 
@@ -29,14 +39,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn egcd() {
-        assert_eq!(super::egcd(102, 38).0, 2);
-        assert_eq!(super::egcd(42823, 6409).0, 17);
-
-        assert_eq!(super::egcd(1914, 899).1, 8);
-        assert_eq!(super::egcd(1914, 899).2, -17);
-
-        assert_eq!(super::egcd(1432, 123211).1, -22973);
-        assert_eq!(super::egcd(1432, 123211).2, 267);
+    fn mod_inv() {
+        assert_eq!(super::mod_inv(Integer::from(7), Integer::from(40)), 23);
+        assert_eq!(super::mod_inv(Integer::from(42), Integer::from(2017)), 1969);
     }
 }
