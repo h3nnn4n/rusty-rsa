bfe9fc1df51beceee45a80ece811d6654dd9fa79
h3nnn4n <uber.renan@gmail.com>
2018-06-02 01:11:16 -0300

    Added bute force factorization

--- src/big_primes.rs
+++ src/big_primes.rs
@@ -6,6 +6,53 @@ use self::rug::ops::Pow;
 use self::rug::rand::RandState;
 use self::rug::Integer;
 
+pub fn prime_factorization_brute_force(x: Integer) -> Vec<Integer> {
+    let mut factors: Vec<Integer> = Vec::new();
+    let mut p = Integer::from(3);
+    let stop = Integer::from(x.clone().sqrt());
+
+    factors.push(Integer::from(1));
+
+    if x.clone() % 2 == 0 {
+        factors.push(Integer::from(2));
+    }
+
+    loop {
+        if x.clone() % p.clone() == 0 {
+            factors.push(p.clone());
+            factors.push(x.clone() / p.clone());
+        }
+
+        p = Integer::from(&p + Integer::from(2));
+
+        if p > stop {
+            break;
+        }
+    }
+
+    factors.push(x.clone());
+
+    factors.sort();
+
+    factors
+}
+
+pub fn prime_factorization_pollard_rho_plus_one(x: Integer) -> Vec<Integer> {
+    let mut factors: Vec<Integer> = Vec::new();
+
+    //x ← 2; y ← 2; d ← 1
+    //while d = 1:
+    //x ← g(x)
+    //y ← g(g(y))
+    //d ← gcd(|x - y|, n)
+    //if d = n:
+    //return failure
+    //else:
+    //return d
+
+    factors
+}
+
 pub fn get_number_with_n_bits(n_bits: i64) -> Integer {
     let consonants = b"01";
     let mut value: Integer;
@@ -178,5 +225,21 @@ mod tests {
     #[test]
     fn primes_less_than_10000() {
         assert!(count_primes(10000) == 1229);
+    }
+
+    #[test]
+    fn brute_force_factorization() {
+        for _ in 0..10 {
+            let m = get_prime_with_n_bits(4);
+            let n = get_prime_with_n_bits(4);
+
+            let p = Integer::from(&m * &n);
+
+            let mut q = vec![Integer::from(1), m, n, p.clone()];
+
+            q.sort();
+
+            assert_eq!(prime_factorization_brute_force(p), q);
+        }
     }
 }
