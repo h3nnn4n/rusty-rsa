158a9df7bc4745c99b5c5ce854f2955a2e3bac79
h3nnn4n <uber.renan@gmail.com>
2018-06-02 23:39:46 -0300

    Wrote script to extract brute forcing runtime

---
+++ scripts/runtime.py
@@ -0,0 +1,53 @@
+from subprocess import Popen, PIPE
+import numpy as np
+
+process = Popen(["cargo", "build", "--release"], stdout=PIPE)
+(output, err) = process.communicate()
+exit_code = process.wait()
+
+t_limit = 20
+repeats = 10
+bits = [ 4 * i for i in range(1, 40)]
+modes = ['pollardrho', 'fermat', 'bruteforce']
+data = {}
+
+for mode in modes:
+    data[mode] = {}
+
+    stop = False
+
+    for bit in bits:
+        data[mode][bit] = []
+
+        print("%20s %5d" % (mode, bit))
+
+        process = Popen(["../target/release/rsa_lixo", "--keysize", str(bit), "--key", "key", "--generate_key"], stdout=PIPE)
+        (output, err) = process.communicate()
+        exit_code = process.wait()
+
+        for r in range(0, repeats):
+            process = Popen(["../target/release/rsa_lixo", "--key", "key.pub", "--%s" % mode], stdout=PIPE)
+            (output, err) = process.communicate()
+            exit_code = process.wait()
+
+            t = float(output.strip())
+
+            if t > t_limit and not stop:
+                stop = True
+                print("Stoping here")
+
+                if bit in data[mode][bit]:
+                    data[mode].pop(bit,None)
+
+                break
+            else:
+                data[mode][bit].append(t)
+
+        if stop:
+            break
+
+
+for mode in modes:
+    for bit in bits:
+        if bit in data[mode]:
+            print("%20s %4d %6.3f %s" % (mode, bit, np.mean(data[mode][bit]), data[mode][bit]))
